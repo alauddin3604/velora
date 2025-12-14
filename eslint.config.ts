32c96e59dce60da674e071c87f54950185cc4656
Alauddin Rofi <alauddin3604@gmail.com>
2025-12-14 20:07:58 +0800

    refactor: add route global to ESLint configuration and format config

--- eslint.config.ts
+++ eslint.config.ts
@@ -11,7 +11,17 @@ export default defineConfig([
     'resources/js/components/ui',
     'vendor/',
   ]),
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,vue}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,vue}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        route: 'readonly',
+      },
+    },
+  },
   tseslint.configs.recommended,
   pluginVue.configs["flat/essential"],
   { files: ["**/*.vue"], languageOptions: { parserOptions: { parser: tseslint.parser } } },
