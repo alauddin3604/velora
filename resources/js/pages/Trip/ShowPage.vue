f9865c2dd253c7474165eba42f86e3ed916de45f
Alauddin Rofi <alauddin3604@gmail.com>
2025-12-20 17:16:13 +0800

    feat: add breadcrumbs

--- resources/js/pages/Trip/ShowPage.vue
+++ resources/js/pages/Trip/ShowPage.vue
@@ -1,13 +1,26 @@
 <script setup lang="ts">
 import DashboardLayout from '@/layouts/DashboardLayout.vue'
-import Trip from '@/types/trip.type'
+import { DashboardBreadcrumb } from '@/types/dashboard-breadcrumb'
+import { Trip } from '@/types/trip.type'
 import { Head } from '@inertiajs/vue3'
+import { ref } from 'vue'
 
-defineProps<{ trip: Trip }>()
+const props = defineProps<{ trip: Trip }>()
+
+const breadcrumbs = ref<DashboardBreadcrumb[]>([
+  {
+    label: 'Trips',
+    url: route('trips.index'),
+  },
+  {
+    label: props.trip.title,
+    url: route('trips.show', props.trip.id),
+  },
+])
 </script>
 
 <template>
-  <DashboardLayout>
+  <DashboardLayout :breadcrumbs="breadcrumbs">
     <Head :title="'Trip - ' + trip.title" />
     <div>
       <h1>{{ trip.title }}</h1>
