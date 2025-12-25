0ba0c7600a222ec01f1f47d00aeee3ec1c7ebedf
Alauddin Rofi <alauddin3604@gmail.com>
2025-12-25 23:09:51 +0800

    chore: setup lando

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,23 @@ import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+        https: {
+            key: '/certs/cert.key',
+            cert: '/certs/cert.crt',
+        },
+        hmr: {
+            host: 'localhost',
+            protocol: 'wss',
+            clientPort: 5173,
+        },
+        cors: {
+            origin: ['https://velora.lndo.site', 'http://velora.lndo.site'],
+            credentials: true,
+        },
+    },
     resolve: {
         alias: {
             'ziggy-js': path.resolve(__dirname, 'vendor/tightenco/ziggy'),
